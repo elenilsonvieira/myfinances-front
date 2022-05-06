eb9f82939aade03d8e3d94ba402ac027f1d1874b
Elenilson <elenilsonvieira@gmail.com>
2022-05-06 16:03:49 -0300

    Criado e utilizado o componente Card e FormGroup

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Login.css';
 
+import Card from '../../components/Card';
+import FormGroup from '../../components/FormGroup';
+
 export default class Login extends React.Component {
 
   state = {
@@ -19,33 +22,28 @@ export default class Login extends React.Component {
         <div className='row'>
           <div className='col-md-6' style={this.styles.colMd6} >
             <div className='bs-docs-section'>
-              <div className='card md-3'>
-                <h3 className='card-header'>Login</h3>
-                <div className='card-body'>
-                    <div className='row'>
-                      <div className='col-lg-12'>
-                        <div className='bs-component'>
-                          <fieldset>
-                            <div className='form-group'>
-                              <label htmlFor='inputEmail'>Email: *</label>
-                              <input type='email' className='form-control' id='inputEmail' 
-                                area-aria-describedby='emailHelp' placeholder='Digite o email'
-                                value={this.state.email} onChange={(e) => this.setState({email: e.target.value})} />
-                            </div>
-                            <div className='form-group'>
-                              <label htmlFor='inputPassword'>Senha: *</label>
-                              <input type='password' className='form-control' id='inputPassword' 
-                                placeholder='Digite a Senha'
-                                value={this.state.password} onChange={(e) => this.setState({password: e.target.value})} />
-                            </div>
-                            <button className='btn btn-success' onClick={this.login}>Entrar</button>
-                            <button className='btn btn-danger'>Cadastrar</button>
-                          </fieldset>
-                        </div>
-                      </div>
+              <Card title='Login'>
+                <div className='row'>
+                  <div className='col-lg-12'>
+                    <div className='bs-component'>
+                      <fieldset>
+                        <FormGroup label='Email: *'>
+                          <input type='email' className='form-control' id='inputEmail' 
+                            area-aria-describedby='emailHelp' placeholder='Digite o email'
+                            value={this.state.email} onChange={(e) => this.setState({email: e.target.value})} />
+                        </FormGroup>
+                        <FormGroup label='Senha: *'>
+                          <input type='password' className='form-control' id='inputPassword' 
+                            placeholder='Digite a Senha'
+                            value={this.state.password} onChange={(e) => this.setState({password: e.target.value})} />
+                        </FormGroup>
+                        <button className='btn btn-success' onClick={this.login}>Entrar</button>
+                        <button className='btn btn-danger'>Cadastrar</button>
+                      </fieldset>
                     </div>
+                  </div>
                 </div>
-              </div>
+              </Card>
             </div>
           </div>
         </div>
