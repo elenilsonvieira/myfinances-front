7a149900e3198f1e2df4723be80fd6cc6c0fdb10
Elenilson <elenilsonvieira@gmail.com>
2022-07-01 22:21:48 -0300

    Colocada a rota autenticada

--- src/main/AppRoutes.js
+++ src/main/AppRoutes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, BrowserRouter } from "react-router-dom";
+import { Route, BrowserRouter, Switch, Redirect } from "react-router-dom";
 
 import Home from "../screens/home/Home";
 import Login from '../screens/login/Login';
@@ -8,17 +8,42 @@ import UpdateUser from "../screens/updateUser/UpdateUser";
 import DeleteUser from "../screens/deleteUser/DeleteUser";
 import ViewUsers from "../screens/viewUsers/ViewUsers";
 
-function AppRoutes() {
+import { AuthConsumer } from '../main/SessionProvider';
+
+function AuthenticatedRoute( { component: Component, isAuthenticated, ...props } ){
+   return (
+       <Route exact {...props} render={ (componentProps) => {
+           if(isAuthenticated){
+               return (
+                   <Component {...componentProps} />
+               )
+           }else{
+               return(
+                   <Redirect to={ {pathname : '/login', state : { from: componentProps.location } } } />
+               )
+           }
+       }}  />
+   )
+}
+
+function AppRoutes(props) {
    return(
       <BrowserRouter>
-         <Route component = { Home }  path="/" exact />
-         <Route component = { Login }  path="/login" />
-         <Route component = { CreateUser }  path="/createUser" />
-         <Route component = { UpdateUser }  path="/updateUser/:id" />
-         <Route component = { DeleteUser }  path="/deleteUser" />
-         <Route component = { ViewUsers }  path="/viewUsers" />
+         <Switch>
+            <Route component = { Home }  path="/" exact />
+            <Route component = { Login }  path="/login" />
+
+            <AuthenticatedRoute isAuthenticated={props.isAuthenticated} component = { CreateUser }  path="/createUser" />
+            <AuthenticatedRoute isAuthenticated={props.isAuthenticated} component = { UpdateUser }  path="/updateUser/:id" />
+            <AuthenticatedRoute isAuthenticated={props.isAuthenticated} component = { DeleteUser }  path="/deleteUser" />
+            <AuthenticatedRoute isAuthenticated={props.isAuthenticated} component = { ViewUsers }  path="/viewUsers" />
+         </Switch>
       </BrowserRouter>
    );
 }
 
-export default AppRoutes;
+export default () => (
+   <AuthConsumer>
+       { (context) => (<AppRoutes isAuthenticated={context.isAuthenticated} />) }
+   </AuthConsumer>
+)
